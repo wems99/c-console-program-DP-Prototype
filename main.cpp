e35294f9ea889b41cdcb56b1a5d1673e603d85f5
TereSolano15 <63326038+TereSolano15@users.noreply.github.com>
2020-10-05 18:07:40 -0600

    main

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Tere Solano on 5/10/2020.
+//
+#include <iostream>
+int main(){
+    std::cout<<"Main"<<std::endl;
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Tere Solano on 5/10/2020.
+//
+#include <iostream>
+
+int main(){
+    std::cout<<"Hola"<<std::endl;
+    return 0;
+}
