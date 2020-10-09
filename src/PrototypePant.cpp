9e16d4523cf21f81374b355c5d63dc3e61c82379
TereSolano15 <63326038+TereSolano15@users.noreply.github.com>
2020-10-08 23:19:01 -0600

    .

--- src/PrototypePant.cpp
+++ src/PrototypePant.cpp
@@ -50,12 +50,12 @@ PrototypePant::~PrototypePant() {
 }
 
 void PrototypePant::printAMassage(char prototypeSize_, string prototypeColor_) {
-
     this->size = prototypeSize_;
     this->color = prototypeColor_;
 
     cout<<"Talla del pantalon: " << prototypeSize_ << " de color "<< prototypeColor_ <<endl;
-    cout<<"el material del pantalon es: " << material <<" marca " << brand <<endl;
+    cout<<"el material del pantalon es: " << material <<endl;
+    cout<<" marca " << brand <<endl;
 
 }
 
